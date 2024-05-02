090c834b7dd078c5e9eaa98543a00507e1ee3e7f
Juttahir <juttahir.moraes@victumservices.com.br>
2024-05-02 09:34:04 -0300

    Refinamento do cadastro de artilheiros

--- assets/js/cadastrar_artilheiros.js
+++ assets/js/cadastrar_artilheiros.js
@@ -1,29 +1,55 @@
-document
-    .querySelector("#botao-cadastrar")
-    .addEventListener("click", (event) => {
-        event.preventDefault()
+document.querySelector("#botao-cadastrar").addEventListener("click", (event) => {
+    event.preventDefault();
 
-        const form = document.querySelector("form")
+    const form = document.querySelector("form");
 
-        const jogador = {
-            imagem: form.imagem.value,
-            nome: form.nome.value,
-            descricao: form.descricao.value,
-            gols: form.gols.value
-        }
+    const jogador = {
+        imagem: form.imagem.value,
+        nome: form.nome.value,
+        descricao: form.descricao.value,
+        gols: form.gols.value
+    };
 
-        console.log(form.nome.value)
+    if (form.checkValidity()) {
+        event.preventDefault();
+        event.stopPropagation();
+        salvar(jogador);
+    } else {
+        event.stopPropagation();
+        form.classList.add('was-validated');
+    }
+});
 
-        if(jogador.imagem && jogador.nome && jogador.descricao && jogador.gols){
-            console.log("Funcionou")
-            salvar(jogador)
-        }
-    })
+function salvar(jogador) {
+    const jogadores = JSON.parse(localStorage.getItem("jogadores")) || [];
+    jogadores.push(jogador);
+    localStorage.setItem("jogadores", JSON.stringify(jogadores));
 
-function salvar(jogador){
-    const jogadores = JSON.parse(localStorage.getItem("jogadores")) || []
-    jogadores.push(jogador)
-    localStorage.setItem("jogadores", JSON.stringify(jogadores))
-
-    window.location = "artilheiros.html"
+    window.location = "artilheiros.html";
 }
+
+const campos = document.querySelectorAll('.needs-validation input');
+
+campos.forEach(function(campo) {
+    campo.addEventListener('input', function() {
+        if (campo.validity.valid) {
+            campo.classList.remove('is-invalid');
+            campo.classList.add('is-valid');
+            campo.nextElementSibling.style.display = 'none';
+        } else {
+            campo.classList.remove('is-valid');
+            campo.classList.add('is-invalid');
+            campo.nextElementSibling.style.display = 'block';
+        }
+    });
+});
+
+document.querySelector('.needs-validation').addEventListener('submit', function(event) {
+    if (this.checkValidity() === false) {
+        event.preventDefault();
+        event.stopPropagation();
+    }
+
+    // Adicione a classe 'was-validated' para mostrar os tooltips de erro
+    this.classList.add('was-validated');
+}, false);
